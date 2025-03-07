91d8fdeba87d7a8968c80f562be969b05a3463ab
SharonU710 <sbosorio@uth.hn>
2025-03-06 20:23:09 -0600

    Add files via upload

---
+++ Main/Deportista.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject4.Main;
+
+/**
+ *
+ * @author s0733
+ */
+class Deportista extends Persona {
+    private String deporte;
+
+    public Deportista(String nombre, int edad, String deporte) {
+        super(nombre, edad);
+        this.deporte = deporte;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Deportista: " + nombre + ", Edad: " + edad + ", Deporte: " + deporte);
+    }
+}
